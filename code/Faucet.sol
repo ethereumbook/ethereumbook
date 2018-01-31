45d261eb8b422e7b07b10e2f95422e0c802c069c
Andreas M. Antonopoulos <andreas@antonopoulos.com>
2018-01-31 14:36:22 -0600

    Even simpler faucet example code

--- code/Faucet.sol
+++ code/Faucet.sol
@@ -4,30 +4,17 @@ pragma solidity ^0.4.19;
 // Our first contract is a faucet!
 contract Faucet {
 
-    // Set a maximum withdrawal amount
-    uint _faucet_withdraw_max_amount = 100 finney;
+	// Give out ether to anyone who asks
+	function withdraw(uint withdraw_amount) public {
 
-    // Initialization function
-    function Faucet() public payable {}
+    	// Limit withdrawal amount
+    	require(withdraw_amount <= 100000000000000000);
 
-    // Get our total balance
-    function getBalance() public view returns (uint) {
-      return this.balance;
+    	// Send the amount to the address that requested it
+    	msg.sender.transfer(withdraw_amount);
     }
 
-    // Give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public {
+	// Accept any incoming amount
+	function () public payable {}
 
-      // Limit withdrawals to a max of _faucet_withdraw_max_amount
-      require(withdraw_amount <= _faucet_withdraw_max_amount);
-
-      // Make sure we have enough to do the transfer
-      require(this.balance >= _faucet_withdraw_max_amount);
-
-      // Send the amount to the address that requested it
-      msg.sender.transfer(withdraw_amount);
-    }
-
-    // Default function - just accept any amount sent
-    function() public payable {}
 }
