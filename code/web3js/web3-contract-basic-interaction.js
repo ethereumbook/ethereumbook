81dee40ba0fff064009f5b392f5804cdaac8ac23
Francisco Javier Rojas García <fjrojasgarcia@gmail.com>
2018-02-11 05:08:13 +0100

    comment deleted

--- code/web3js/web3-contract-basic-interaction.js
+++ code/web3js/web3-contract-basic-interaction.js
@@ -89,7 +89,6 @@ console.log(etherescan_url);
 var client = require('node-rest-client-promise').Client();
 
 // Now we are going to deal with the contract from web3.js in a non-block fashion (async mode)
-// client.getPromise("http://kovan.etherscan.io/api?module=contract&action=getabi&address=" + our_contract_address)
 client.getPromise(etherescan_url)
 .then((client_promise) => {
   // Leave this two lines for fure object analisys
