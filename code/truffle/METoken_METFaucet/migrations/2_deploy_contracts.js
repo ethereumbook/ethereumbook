458edc42845d35d112dcdd9c147a025f186864d6
Leo Arias <yo@elopio.net>
2018-03-17 13:05:42 -0500

    fix typos found by codespell

--- code/truffle/METoken_METFaucet/migrations/2_deploy_contracts.js
+++ code/truffle/METoken_METFaucet/migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = function(deployer) {
 
 	// Deploy the METoken contract first
 	deployer.deploy(METoken, {from: owner}).then(function() {
-		// then deploy METFaucet and pass the addres of METoken
+		// then deploy METFaucet and pass the address of METoken
 		// and the address of the owner of all the MET who will approve METFaucet
 		return deployer.deploy(METFaucet, METoken.address, owner);
   	});
