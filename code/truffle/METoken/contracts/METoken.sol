5432758fb1c76fa932f16b0e27282344552a22c3
Vignesh Karthikeyan <vignesh@alumni.purdue.edu>
2018-09-08 23:39:03 -0400

    Constructor changed to match current standard
    Same-name constructor throws warning of deprecation

--- code/truffle/METoken/contracts/METoken.sol
+++ code/truffle/METoken/contracts/METoken.sol
@@ -8,7 +8,7 @@ contract METoken is StandardToken {
     uint8 public constant decimals = 2;
     uint constant _initial_supply = 2100000000;
 
-    function METoken() public {
+    constructor() public {
         totalSupply_ = _initial_supply;
         balances[msg.sender] = _initial_supply;
         emit Transfer(address(0), msg.sender, _initial_supply);
