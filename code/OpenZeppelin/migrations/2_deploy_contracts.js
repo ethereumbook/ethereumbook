a39c42dcc3d3cd0ca6281be2c9c69e6501d70b3e
Nick Adams <nadams@oreilly.com>
2018-10-11 17:14:06 -0400

    fixing long code line in App D

--- code/OpenZeppelin/migrations/2_deploy_contracts.js
+++ code/OpenZeppelin/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const SampleCrowdsale = artifacts.require('./SampleCrowdsale.sol');
 const SampleToken = artifacts.require('./SampleToken.sol');
 
 module.exports = function(deployer, network, accounts) {
-  const openingTime = web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
+  const openingTime = web3.eth.getBlock('latest').timestamp + 2; // 2s in future
   const closingTime = openingTime + 86400 * 20; // 20 days
   const rate = new web3.BigNumber(1000);
   const wallet = accounts[1];
