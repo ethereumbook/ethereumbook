6d359fd55140bf64c07cbb8798ccb3f0e9eb1c87
Oliver Bristow <oliver.bristow@project-tracr.com>
2019-09-12 07:52:58 +0100

    Homogenise leading whitespace in Solidarity examples

--- code/Solidity/Faucet3.sol
+++ code/Solidity/Faucet3.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.4.22;
 // Our first contract is a faucet!
 contract Faucet {
 
-	address owner;
+    address owner;
 
-	// Contract constructor: set owner
-	constructor() {
-		owner = msg.sender;
-	}
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
 
-	// Contract destructor
-	function destroy() public {
-		require(msg.sender == owner);
-		selfdestruct(owner);
-	}
+    // Contract destructor
+    function destroy() public {
+        require(msg.sender == owner);
+        selfdestruct(owner);
+    }
 
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
 
         // Limit withdrawal amount
-		require(withdraw_amount <= 0.1 ether);
+        require(withdraw_amount <= 0.1 ether);
 
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
--- code/Solidity/Faucet4.sol
+++ code/Solidity/Faucet4.sol
@@ -4,29 +4,29 @@ pragma solidity ^0.4.22;
 // Our first contract is a faucet!
 contract Faucet {
 
-	address owner;
+    address owner;
 
-	// Contract constructor: set owner
-	constructor() {
-		owner = msg.sender;
-	}
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
 
-	// Access control modifier
-	modifier onlyOwner {
-	    require(msg.sender == owner);
-	    _;
-	}
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-	// Contract destructor
-	function destroy() public onlyOwner {
-		selfdestruct(owner);
-	}
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
 
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
 
         // Limit withdrawal amount
-		require(withdraw_amount <= 0.1 ether);
+        require(withdraw_amount <= 0.1 ether);
 
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
