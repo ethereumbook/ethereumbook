b79f59a6e2c768b1207aeebd87cd63ef23b69ade
Will Binns <hello@willbinns.org>
2020-01-12 11:42:20 +0100

    code: Ensure proper use of 'web3.utils.toWei'

--- code/truffle/FaucetEvents/gas_estimates.js
+++ code/truffle/FaucetEvents/gas_estimates.js
@@ -8,8 +8,8 @@ FaucetContract.web3.eth.getGasPrice(function(error, result) {
     FaucetContract.deployed().then(function(FaucetContractInstance) {
 
         // Use the keyword 'estimateGas' after the function name to get the gas estimation for this particular function (aprove)
-		FaucetContractInstance.send(web3.toWei(1, "ether"));
-        return FaucetContractInstance.withdraw.estimateGas(web3.toWei(0.1, "ether"));
+		FaucetContractInstance.send(web3.utils.toWei(1, "ether"));
+        return FaucetContractInstance.withdraw.estimateGas(web3.utils.toWei(0.1, "ether"));
 
     }).then(function(result) {
         var gas = Number(result);
