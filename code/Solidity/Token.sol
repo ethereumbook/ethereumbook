9775cc7b766c2531e6723deeb48ad472b2638a38
HAOYUatHZ <haoyu@protonmail.com>
2020-01-25 22:19:35 +0800

    style: lint Token.sol

--- code/Solidity/Token.sol
+++ code/Solidity/Token.sol
@@ -1,11 +1,10 @@
 import "Faucet.sol"
 
 contract Token is mortal {
+    Faucet _faucet;
 
-	Faucet _faucet;
-
-	constructor(address _f) {
-		_faucet = Faucet(_f);
-		_faucet.withdraw(0.1 ether)
-	}
+    constructor(address _f) {
+        _faucet = Faucet(_f);
+        _faucet.withdraw(0.1 ether)
+    }
 }
