202f63a268c8706420dce4833603ab58fbfb475c
HAOYUatHZ <haoyu@protonmail.com>
2020-01-25 21:57:34 +0800

    style: more fixes on Blank Lines

--- code/auction_dapp/backend/contracts/ERC721/ERC721Basic.sol
+++ code/auction_dapp/backend/contracts/ERC721/ERC721Basic.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.18;
 
-
 /**
  * @title ERC721 Non-Fungible Token Standard basic interface
  * @dev see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
--- code/auction_dapp/backend/contracts/ERC721/ERC721Receiver.sol
+++ code/auction_dapp/backend/contracts/ERC721/ERC721Receiver.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.18;
 
-
 /**
  * @title ERC721 token receiver interface
  * @dev Interface for any contract that wants to support safeTransfers
