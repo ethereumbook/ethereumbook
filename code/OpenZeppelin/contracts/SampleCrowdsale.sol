be4222293a8c4ecc5ad2a1626f9f18c686924819
HAOYUatHZ <haoyu@protonmail.com>
2020-01-25 22:02:40 +0800

    style: lint OpenZeppelin contracts

--- code/OpenZeppelin/contracts/SampleCrowdsale.sol
+++ code/OpenZeppelin/contracts/SampleCrowdsale.sol
@@ -3,20 +3,20 @@ pragma solidity 0.4.23;
 import './SampleToken.sol';
 import 'openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
 import 'openzeppelin-solidity/contracts/crowdsale/ \
-  distribution/PostDeliveryCrowdsale.sol';
+    distribution/PostDeliveryCrowdsale.sol';
+
 
 contract SampleCrowdsale is PostDeliveryCrowdsale, MintedCrowdsale {
-
-  constructor(
-    uint256 _openingTime,
-    uint256 _closingTime
-    uint256 _rate,
-    address _wallet,
-    MintableToken _token
-  )
-    public
-    Crowdsale(_rate, _wallet, _token)
-    PostDeliveryCrowdsale(_openingTime, _closingTime)
-  {
-  }
+    constructor(
+        uint256 _openingTime,
+        uint256 _closingTime
+        uint256 _rate,
+        address _wallet,
+        MintableToken _token
+    )
+        public
+        Crowdsale(_rate, _wallet, _token)
+        PostDeliveryCrowdsale(_openingTime, _closingTime)
+    {
+    }
 }
--- code/OpenZeppelin/contracts/SampleToken.sol
+++ code/OpenZeppelin/contracts/SampleToken.sol
@@ -2,8 +2,9 @@ pragma solidity 0.4.23;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
+
 contract SampleToken is MintableToken {
-  string public name = "SAMPLE TOKEN";
-  string public symbol = "SAM";
-  uint8 public decimals = 18;
+    string public name = "SAMPLE TOKEN";
+    string public symbol = "SAM";
+    uint8 public decimals = 18;
 }
