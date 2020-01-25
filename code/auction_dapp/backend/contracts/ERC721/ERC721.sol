f6067e7009582e41a18f6b6bb2747df0496230d7
HAOYUatHZ <haoyu@protonmail.com>
2020-01-25 15:35:20 +0800

    style: lint ERC721.sol in `auction_dapp`

--- code/auction_dapp/backend/contracts/ERC721/ERC721.sol
+++ code/auction_dapp/backend/contracts/ERC721/ERC721.sol
@@ -8,9 +8,9 @@ import "./ERC721Basic.sol";
  * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
 contract ERC721Enumerable is ERC721Basic {
-  function totalSupply() public view returns (uint256);
-  function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256 _tokenId);
-  function tokenByIndex(uint256 _index) public view returns (uint256);
+    function totalSupply() public view returns (uint256);
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256 _tokenId);
+    function tokenByIndex(uint256 _index) public view returns (uint256);
 }
 
 
@@ -19,9 +19,9 @@ contract ERC721Enumerable is ERC721Basic {
  * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
 contract ERC721Metadata is ERC721Basic {
-  function name() public view returns (string _name);
-  function symbol() public view returns (string _symbol);
-  function tokenURI(uint256 _tokenId) public view returns (string);
+    function name() public view returns (string _name);
+    function symbol() public view returns (string _symbol);
+    function tokenURI(uint256 _tokenId) public view returns (string);
 }
 
 
