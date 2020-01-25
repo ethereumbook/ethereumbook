dbca8e492424b7534987063f3063aa41bb63621b
HAOYUatHZ <haoyu@protonmail.com>
2020-01-25 22:22:11 +0800

    style: lint Faucet2.sol

--- code/Solidity/Faucet2.sol
+++ code/Solidity/Faucet2.sol
@@ -3,18 +3,15 @@ pragma solidity ^0.4.19;
 
 // Our first contract is a faucet!
 contract Faucet {
+    // Accept any incoming amount
+    function () external payable {}
 
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
-
         // Limit withdrawal amount
         require(withdraw_amount <= 100000000000000000);
 
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
     }
-
-    // Accept any incoming amount
-    function () external payable {}
-
 }
