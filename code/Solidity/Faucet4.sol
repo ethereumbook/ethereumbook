30edb8270e5a57d6c9e0056937c90e157d5baf26
HAOYUatHZ <haoyu@protonmail.com>
2020-01-26 11:40:27 +0800

    style: lint Faucet4.sol

--- code/Solidity/Faucet4.sol
+++ code/Solidity/Faucet4.sol
@@ -3,36 +3,33 @@ pragma solidity ^0.4.22;
 
 // Our first contract is a faucet!
 contract Faucet {
+    address owner;
 
-	address owner;
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
 
-	// Contract constructor: set owner
-	constructor() {
-		owner = msg.sender;
-	}
-
-	// Access control modifier
-	modifier onlyOwner {
-	    require(msg.sender == owner);
-	    _;
-	}
-
-	// Contract destructor
-	function destroy() public onlyOwner {
-		selfdestruct(owner);
-	}
-
-    // Give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public {
-
-        // Limit withdrawal amount
-		require(withdraw_amount <= 0.1 ether);
-
-        // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
     }
 
     // Accept any incoming amount
     function () external payable {}
 
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        // Limit withdrawal amount
+        require(withdraw_amount <= 0.1 ether);
+
+        // Send the amount to the address that requested it
+        msg.sender.transfer(withdraw_amount);
+    }
 }
