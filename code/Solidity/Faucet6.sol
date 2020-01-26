f3e2a793c40563b290686bbd98832c9b29d46105
HAOYUatHZ <haoyu@protonmail.com>
2020-01-26 15:06:24 +0800

    style: lint Faucet6.sol

--- code/Solidity/Faucet6.sol
+++ code/Solidity/Faucet6.sol
@@ -1,34 +1,38 @@
 // Version of Solidity compiler this program was written for
 pragma solidity ^0.4.22;
 
-contract owned {
-	address owner;
-	// Contract constructor: set owner
-	constructor() {
-		owner = msg.sender;
-	}
-	// Access control modifier
-	modifier onlyOwner {
-	    require(msg.sender == owner, "Only the contract owner can call this function");
-	    _;
-	}
+contract Owned {
+    address owner;
+
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
 }
 
-contract mortal is owned {
-	// Contract destructor
-	function destroy() public onlyOwner {
-		selfdestruct(owner);
-	}
+contract Mortal is Owned {
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
 
-contract Faucet is mortal {
+contract Faucet is Mortal {
+    // Accept any incoming amount
+    function () external payable {}
+
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
-		require(withdraw_amount <= 0.1 ether);
+        require(withdraw_amount <= 0.1 ether);
+
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
     }
-    // Accept any incoming amount
-    function () external payable {}
 }
