41b0e0301c55aa8682931825b8985005e1cbcae5
HAOYUatHZ <haoyu@protonmail.com>
2020-01-26 15:13:40 +0800

    style: lint Faucet8.sol

--- code/Solidity/Faucet8.sol
+++ code/Solidity/Faucet8.sol
@@ -1,43 +1,50 @@
 // Version of Solidity compiler this program was written for
 pragma solidity ^0.4.22;
 
-contract owned {
-	address owner;
-	// Contract constructor: set owner
-	constructor() {
-		owner = msg.sender;
-	}
-	// Access control modifier
-	modifier onlyOwner {
-		require(msg.sender == owner,
-		        "Only the contract owner can call this function");
-		_;
-	}
+contract Owned {
+    address owner;
+
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
 }
 
-contract mortal is owned {
-	// Contract destructor
-	function destroy() public onlyOwner {
-		selfdestruct(owner);
-	}
+contract Mortal is Owned {
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
 
-contract Faucet is mortal {
-	event Withdrawal(address indexed to, uint amount);
-	event Deposit(address indexed from, uint amount);
+contract Faucet is Mortal {
+    event Withdrawal(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);
 
-	// Give out ether to anyone who asks
-	function withdraw(uint withdraw_amount) public {
-		// Limit withdrawal amount
-		require(withdraw_amount <= 0.1 ether);
-		require(this.balance >= withdraw_amount,
-			"Insufficient balance in faucet for withdrawal request");
-		// Send the amount to the address that requested it
-		msg.sender.transfer(withdraw_amount);
-		emit Withdrawal(msg.sender, withdraw_amount);
-	}
-	// Accept any incoming amount
-	function () external payable {
-		emit Deposit(msg.sender, msg.value);
-	}
+    // Accept any incoming amount
+    function () external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        // Limit withdrawal amount
+        require(withdraw_amount <= 0.1 ether);
+
+        require(
+            this.balance >= withdraw_amount,
+            "Insufficient balance in faucet for withdrawal request"
+        );
+
+        // Send the amount to the address that requested it
+        msg.sender.transfer(withdraw_amount);
+
+        emit Withdrawal(msg.sender, withdraw_amount);
+    }
 }
