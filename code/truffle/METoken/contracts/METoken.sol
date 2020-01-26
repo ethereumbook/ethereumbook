548095f533eb73b28f20d8287fded4928803a5fd
HAOYUatHZ <haoyu@protonmail.com>
2020-01-26 20:46:11 +0800

    style: lint METoken.sol

--- code/truffle/METoken/contracts/METoken.sol
+++ code/truffle/METoken/contracts/METoken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.21;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
+
 contract METoken is StandardToken {
     string public constant name = 'Mastering Ethereum Token';
     string public constant symbol = 'MET';
