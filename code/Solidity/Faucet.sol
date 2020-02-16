69e8319f2edba1f87fbad0203e2c1a3239781e33
Danger <34241475+safetyth1rd@users.noreply.github.com>
2020-02-16 14:21:25 -0800

    Update faucet.sol to solc v0.6
    -pragma ^0.6 for solc forward compatibility -fallback function should now be receive() -function order should be: constructor -> external -> public -> internal -> private
    Changes based on @HAOYUatHZ Pull request #931

--- code/Solidity/Faucet.sol
+++ code/Solidity/Faucet.sol
@@ -1,9 +1,11 @@
 // Version of Solidity compiler this program was written for
-pragma solidity 0.5.12;
+pragma solidity ^0.6.0;
 
 // Our first contract is a faucet!
 contract Faucet {
-
+    // Accept any incoming amount
+    function () external payable {}
+    
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
 
@@ -13,8 +15,4 @@ contract Faucet {
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
     }
-
-    // Accept any incoming amount
-    function () external payable {}
-
 }
