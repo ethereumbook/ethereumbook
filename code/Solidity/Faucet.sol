4c791aded69623a712e3e61b593d609dce7b4b84
Danger <34241475+safetyth1rd@users.noreply.github.com>
2020-02-16 23:06:27 -0800

    fixed typo - function ->receive

--- code/Solidity/Faucet.sol
+++ code/Solidity/Faucet.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.0;
 // Our first contract is a faucet!
 contract Faucet {
     // Accept any incoming amount
-    function () external payable {}
+    receive () external payable {}
     
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
