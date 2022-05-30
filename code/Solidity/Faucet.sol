806e0f400d298820d93bcb12e5495b40e01b57ff
kirby88 <65531550+kirby88@users.noreply.github.com>
2022-05-30 15:01:36 +0100

    Update Faucet.sol
    Marking the sender address as 'payable'

--- code/Solidity/Faucet.sol
+++ code/Solidity/Faucet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC-BY-SA-4.0
 
 // Version of Solidity compiler this program was written for
-pragma solidity 0.6.4;
+pragma solidity 0.8.7;
 
 // Our first contract is a faucet!
 contract Faucet {
@@ -14,6 +14,6 @@ contract Faucet {
         require(withdraw_amount <= 100000000000000000);
 
         // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
     }
 }
