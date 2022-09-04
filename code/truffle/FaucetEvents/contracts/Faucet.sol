28467e7f2383cea69e17842dacb6c9fbeb381f38
AnandK-2024 <anandkumar@iitbhilai.ac.in>
2022-09-04 13:22:56 +0530

    change solidity version and update function of incoming account

--- code/truffle/FaucetEvents/contracts/Faucet.sol
+++ code/truffle/FaucetEvents/contracts/Faucet.sol
@@ -1,42 +1,51 @@
+// SPDX-License-Identifier: MIT
 // Version of Solidity compiler this program was written for
-pragma solidity ^0.4.22;
+pragma solidity >=0.4.0 <0.9.0;
 
 contract owned {
-	address owner;
-	// Contract constructor: set owner
-	constructor() {
-		owner = msg.sender;
-	}
-	// Access control modifier
-	modifier onlyOwner {
-	    require(msg.sender == owner, "Only the contract owner can call this function");
-	    _;
-	}
+    address owner;
+
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Access control modifier
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function"
+        );
+        _;
+    }
 }
 
 contract mortal is owned {
-	// Contract destructor
-	function destroy() public onlyOwner {
-		selfdestruct(owner);
-	}
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(payable(owner));
+    }
 }
 
 contract Faucet is mortal {
-	event Withdrawal(address indexed to, uint amount);
-	event Deposit(address indexed from, uint amount);
+    event Withdrawal(address indexed to, uint256 amount);
+    event Deposit(address indexed from, uint256 amount);
 
     // Give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public {
+    function withdraw(uint256 withdraw_amount) public {
         // Limit withdrawal amount
-		require(withdraw_amount <= 0.1 ether);
-		require(this.balance >= withdraw_amount,
-			"Insufficient balance in faucet for withdrawal request");
+        require(withdraw_amount <= 0.1 ether);
+        require(
+            address(this).balance >= withdraw_amount,
+            "Insufficient balance in faucet for withdrawal request"
+        );
         // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
-		emit Withdrawal(msg.sender, withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
+        emit Withdrawal(msg.sender, withdraw_amount);
     }
+
     // Accept any incoming amount
-    function () external payable {
-		emit Deposit(msg.sender, msg.value);
-	}
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 }
